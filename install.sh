c97de56698593ad1024b12a7fe1bc85aa914b144
VENOMxCRAZYy <venomcrazy904@gmail.com>
2024-08-02 23:29:37 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ cat <<EOL > .devcontainer/devcontainer.json
 {
     "name": "My Codespace",
     "image": "mcr.microsoft.com/vscode/devcontainers/python:3.8",
-    "postStartCommand": "python3 /workspaces/repo-name/main.py",
+    "postStartCommand": "python3 /workspaces/venom-V5-Testing/main.py",
     "customizations": {
         "vscode": {
             "settings": {
